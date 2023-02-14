c8c8a941adfd6caa1457b19b06eefb78bfaba56c
Allakazan <brunorizzardi80014@hotmail.com>
2023-02-14 00:36:48 -0300

    fix: dead cdn url

--- src/Service/AssetManagement/config.js
+++ src/Service/AssetManagement/config.js
@@ -4,7 +4,7 @@ export default {
             'https://cdnjs.cloudflare.com/ajax/libs/baffle.js/0.3.6/baffle.min.js',
             'https://unpkg.com/aos@2.3.1/dist/aos.js',
             'https://threejs.org/build/three.min.js',
-            'https://threejs.org/examples/js/effects/AnaglyphEffect.js',
+            './public/assets/js/imports/three.anaglyph.js',
             'https://cdn.jsdelivr.net/npm/@splidejs/splide@latest/dist/js/splide.min.js',
             'https://unpkg.com/isotope-layout@3/dist/isotope.pkgd.min.js',
             './public/assets/js/modules/Baffle.js',
